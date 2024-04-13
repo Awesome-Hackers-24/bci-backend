d9f2210bc5e86c35b4330a3504debf93a1056de3
cosmiE-3 <78379787+cosmiE-3@users.noreply.github.com>
2024-04-13 16:37:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import os
+from pyOpenBCI import OpenBCICyton
 from supabase import create_client, Client
+
+def print_raw(sample):
+    data = {
+        'pos1': sample.channels_data[0],
+        'pos2': sample.channels_data[1],
+        'pos3': sample.channels_data[2],
+        'pos4': sample.channels_data[3],
+        'pos5': sample.channels_data[4],
+        'pos6': sample.channels_data[5],
+        'pos7': sample.channels_data[6],
+    }
+    supabase.table('sensor_data').update(data).eq('id', 1).execute()
+
+board = OpenBCICyton(port='COM3', daisy=False)
 
 def print_hi(name):
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
-
 if __name__ == '__main__':
     print_hi('PyCharm')
+    board.start_stream(print_raw)
 
 
 url: str = os.environ.get("https://sfesooveqwsgfzkosmxu.supabase.co")
